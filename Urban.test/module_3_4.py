5fdb0e4a08c29667a8b20880125200e1beb21037
RicHiPPH <kataws75@icloud.com>
2024-11-02 19:14:46 +0200

    Первый коммит

--- Urban.test/module_3_4.py
+++ Urban.test/module_3_4.py
@@ -1,13 +1,14 @@
-# Airborne, aIrplane, fareair, Backing, Backward
+
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if root_word.startswith(i) or i.startswith(root_word):
+        if root_word.lower() in i.lower()  \
+                or i.lower() in root_word.lower():
             same_words.append(i)
     return same_words
 
 
-print(single_root_words("air", "aIrplane", "fareair", "Backing", "Backward"))
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies', 'manirichi','goldriCH'))
 print(single_root_words('Disablement', "Able", "Mable", "Disable", "Bagel"))
 
-# slou = ("aIrplane", "fareair", "Backing", "Backward")
+
