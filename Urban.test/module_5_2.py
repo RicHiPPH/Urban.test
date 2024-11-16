bef39ce505b5d2da2a5b1b341e0890b6bc8a3890
RicHiPPH <kataws75@icloud.com>
2024-11-16 19:15:55 +0200

    .

---
+++ Urban.test/module_5_2.py
@@ -0,0 +1,27 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = int(new_floor)
+        floor = 0
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print(f'{new_floor} - такого этажа не существует')
+        else:
+            for floor in range(new_floor):
+                print(floor + 1)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+         return 'Название: {0}, кол-во этажей: {1}' .format(self.name, self.number_of_floors)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
