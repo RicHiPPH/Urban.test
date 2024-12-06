98873a05e5d7385fddb06290a111094af85b1990
RicHiPPH <kataws75@icloud.com>
2024-12-06 16:47:46 +0200

    .

--- openfile/openfile.py
+++ openfile/openfile.py
@@ -1,12 +1,18 @@
 from pprint import pprint
-
-
-#name = "sample.txt"
-#file = open(name, "r") #r,w,a read, write, append
-#pprint(file.read())
-#file.close()
+import io
 
 name = "sample2.txt"
-file = open(name, "a") #r,w,a read, write, append
-file.write("\nHello world2")
+file = open(name, "r", encoding="utf-8") #r,w,a read, write, append
+print(file.writable())
+print(file.readable())
+print(file.seekable())
+print(file.closed)
+pprint(file.tell())
+pprint(file.read())
+pprint(file.tell())
 file.close()
+
+#name = "sample2.txt"
+#file = open(name, "a") #r,w,a read, write, append
+#file.write("\nHello world2")
+#file.close()
